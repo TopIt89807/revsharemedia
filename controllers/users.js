fb6bdc09564a05cff0e69c8501dcd90d3b3530a8
TopIT Dev <topit89807@gmail.com>
2018-02-22 16:08:45 +0400

    completed user signup api

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,28 @@
 const users = require('../models/users');
 const config = require('../config/config.json');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res) => {
-    res.status(200).json({ message: 'Success '});
+    const {email, password} = req.body;
+
+    users.find({email: email})
+    .then((result) => {
+        if(result.length == 0) {
+            const salt = bcrypt.genSaltSync();
+            const hash = bcrypt.hashSync(password, salt);
+
+            const newUser = new users({
+                email,
+                hashed_password: hash
+            })
+            newUser.save();
+            res.status(201).json({ message: 'User registered successfully!'});
+        } else {
+            res.status(409).json({ message: 'User already registered!'});
+        }
+    })
+    .catch((err) => {
+        res.status(500).json({ err });
+    });
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../../controllers/users');
+var validate = require('express-validation');
+var Joi = require('joi');
 
-router.post('/signup', users.signup);
+router.post('/signup', validate(
+    {
+        body: {
+            email: Joi.string().email().required(),
+            password: Joi.string().required()
+        }
+    }
+), users.signup);
 
 module.exports = router;
